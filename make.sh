894504f7d1ea227f8ab743937fe627fb9d42c656
coolnet <1516070515@qq.com>
2020-06-10 16:35:36 +0800

    修复错误

--- make.sh
+++ make.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sed -e 's/,function(window,document,Laya){.*$//' main_001357.js > 1.js
-sed -e 's/^.*,function(window,document,Laya){/(function(window,document,Laya){/'  main_001357.js >2.js
+sed -e 's/^.*,function(window,document,Laya){/__a=1,function(window,document,Laya){/'  main_001357.js >2.js
 sed -i 's/var BaseAction/BaseAction/' 2.js 
 sed -i 's/FG.mainConnecter.send(t/window.HookSendMsg\&\&window.HookSendMsg(t,e)\&\&FG.mainConnecter.send(t/' 2.js
 sed -i 's/s.setMessage(i)/window.HookRecvMsg\&\&window.HookRecvMsg(t,i,s)\&\&s.setMessage(i)/' 2.js
-sed -i 's/}(window,document,Laya)/})(window,document,Laya)/' 2.js
+# sed -i 's/}(window,document,Laya)/})(window,document,Laya)/' 2.js
 
